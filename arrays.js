d5455887a0a2cd7be8bbc09069c4fbf526456366
naizal Edy <pritiadhikari@gmail.com>
2018-06-26 23:33:04 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -44,8 +44,8 @@ function destructivelyRemoveElementFromBeginningOfArray(array)
 function removeElementFromBeginningOfArray(array)
 {
   
-  array.slice(0)
-  return array
+ 
+  return  array.slice(1)
 }
 
 function destructivelyRemoveElementFromEndOfArray(array)
